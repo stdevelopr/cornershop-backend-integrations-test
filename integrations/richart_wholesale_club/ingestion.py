829b44f92ee1cb64b4537788b17ca83f6fd6ef62
richard <richard.turra@sensedata.com.br>
2020-11-14 03:32:26 -0300

    price_stock csv mapped to db

--- integrations/richart_wholesale_club/ingestion.py
+++ integrations/richart_wholesale_club/ingestion.py
@@ -18,7 +18,6 @@ def process_csv_files():
     products_df = pd.read_csv(filepath_or_buffer=PRODUCTS_PATH, sep="|",)
     prices_stock_df = pd.read_csv(filepath_or_buffer=PRICES_STOCK_PATH, sep="|",)
 
-    # insert the products in the table
     products = []
     for index, row in products_df.iterrows():
         if not pd.isnull(row["NAME"]):
@@ -38,6 +37,34 @@ def process_csv_files():
         session.commit()
     except Exception as e:
         print("PRODUCTS error", e)
+        session.rollback()
+
+
+    # create a map between sku and id for each product
+    products = session.query(Product).with_entities(Product.id, Product.sku)
+    sku_id_map = {product[0]:product[1] for product in products}
+
+    # clean null values
+    if prices_stock_df.isnull().values.sum() > 0:
+        print("Number of rows with null values (Not inserted): ", prices_stock_df.isnull().values.sum())
+        prices_stock_df = prices_stock_df.dropna(how='any',axis=0)
+
+    prices_stock = []
+    for index, row in prices_stock_df.iterrows():
+        try:
+            branch_product = BranchProduct(product_id= sku_id_map[row["SKU"]])
+            branch_product.branch = row["BRANCH"]
+            branch_product.stock = row["STOCK"]
+            branch_product.price = row["PRICE"]
+            prices_stock.append(branch_product)
+        except Exception as e:
+            print("PRICE-STOCK error", e)
+
+    try:
+        session.bulk_save_objects(prices_stock)
+        session.commit()
+    except Exception as e:
+        print("error", e)
         session.rollback()
 
 
