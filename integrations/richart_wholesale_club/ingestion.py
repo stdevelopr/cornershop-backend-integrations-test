5d3a6a72d9479cca4fe9c35a332392a46942c4af
richard <richard.turra@sensedata.com.br>
2020-11-16 01:52:13 -0300

    logging integration

--- integrations/richart_wholesale_club/ingestion.py
+++ integrations/richart_wholesale_club/ingestion.py
@@ -5,6 +5,10 @@ import pandas as pd
 
 from models import BranchProduct, Product
 from integrations import Session
+import logging
+
+logging.basicConfig(filename='integrations/integrations.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 session = Session()
 
@@ -15,10 +19,26 @@ PRICES_STOCK_PATH = os.path.join(ASSETS_DIR, "PRICES-STOCK.csv")
 
 
 def process_csv_files():
-    products_df = pd.read_csv(filepath_or_buffer=PRODUCTS_PATH, sep="|",)
-    prices_stock_df = pd.read_csv(filepath_or_buffer=PRICES_STOCK_PATH, sep="|",)
+    """Process csv files"""
+    try:
+        products_df = pd.read_csv(filepath_or_buffer=PRODUCTS_PATH, sep="|",)
+        prices_stock_df = pd.read_csv(filepath_or_buffer=PRICES_STOCK_PATH, sep="|",)
+    except Exception as e:
+        logging.error(f"Error reading file, {e}")
 
+    # process_products_file(products_df)
+
+    # create a map between sku and id for each product
+    products = session.query(Product).with_entities(Product.id, Product.sku)
+    sku_id_map = {product[0]:product[1] for product in products}
+
+    process_stock(prices_stock_df, sku_id_map)
+
+
+def process_products_file(products_df: pd.DataFrame):
+    """Process the products file"""
     products = []
+    logging.info("Processing PRODUCTS.csv file...")
     for index, row in products_df.iterrows():
         if not pd.isnull(row["NAME"]):
             product = Product(sku=row["SKU"], store="Richart's")
@@ -31,40 +51,42 @@ def process_csv_files():
             product.name = row["NAME"]
             products.append(product)
         else:
-            print("PRODUCTS | Product without name! index:", index)
+            logging.warning(f"PRODUCTS | Product without name! will not be included - index: {index}")
     try:
         session.bulk_save_objects(products)
         session.commit()
     except Exception as e:
-        print("PRODUCTS error", e)
+        logging.exception(f"PRODUCTS error. msg: {e}")
         session.rollback()
 
-
-    # create a map between sku and id for each product
-    products = session.query(Product).with_entities(Product.id, Product.sku)
-    sku_id_map = {product[0]:product[1] for product in products}
-
-    # clean null values
+def process_stock(prices_stock_df: pd.DataFrame, sku_id_map: dict):
+    """Process the stock prices file"""
+    logging.info("Processing PRICES-STOCK file...")
+    # cleaning null values, since nullable is false for all columns
     if prices_stock_df.isnull().values.sum() > 0:
-        print("Number of rows with null values (Not inserted): ", prices_stock_df.isnull().values.sum())
+        logging.warning(f"Number of rows with null values (will not be included): {prices_stock_df.isnull().values.sum()}")
         prices_stock_df = prices_stock_df.dropna(how='any',axis=0)
-
     prices_stock = []
     for index, row in prices_stock_df.iterrows():
         try:
-            branch_product = BranchProduct(product_id= sku_id_map[row["SKU"]])
+            product_id = sku_id_map[row["SKU"]]
+        except Exception as e:
+            logging.error(f"PRICE-STOCK | could not map product id and sku: {e}")
+
+        try:
+            branch_product = BranchProduct(product_id= product_id)
             branch_product.branch = row["BRANCH"]
             branch_product.stock = row["STOCK"]
             branch_product.price = row["PRICE"]
             prices_stock.append(branch_product)
         except Exception as e:
-            print("PRICE-STOCK error", e)
+            logging.error(f"PRICE-STOCK | error instanciating BranchProduct: {e}")
 
     try:
         session.bulk_save_objects(prices_stock)
         session.commit()
     except Exception as e:
-        print("error", e)
+        logging.exception(f"PRICES-STOCK error. {e}")
         session.rollback()
 
 
